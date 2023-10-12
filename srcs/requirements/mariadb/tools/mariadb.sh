756720599cc502ee25824827608612f0c30fa1ed
ThEWoLf882-42 <agimi@student.1337.ma>
2023-10-12 15:50:52 +0100

    some thing

---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+mysql_install_db
+
+/etc/init.d/mysql start
+
+
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then 
+
+	echo "Database already exists"
+else
+
+mysql_secure_installation << _EOF_
+
+Y
+root4life
+root4life
+Y
+n
+Y
+Y
+_EOF_
+
+
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+
+mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+
+fi
+
+/etc/init.d/mysql stop
+
+exec "$@"
