0fa17753c445fc50ab1321b2187dbe9c4117c74c
ThEWoLf882-42 <agimi@student.1337.ma>
2023-10-20 15:53:57 +0100

    ff

--- dir.sh
+++ dir.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
-mkdir project
-mkdir project/srcs
-touch project/Makefile
-mkdir project/srcs/requirements
-touch project/srcs/docker-compose.yml
-touch project/srcs/.env
-echo "DOMAIN_NAME=agimi.42.fr" > project/srcs/.env
-echo "CERT_=./requirements/tools/agimi.42.fr.crt" >> project/srcs/.env
-echo "KEY_=./requirements/tools/agimi.42.fr.key" >> project/srcs/.env
-echo "DB_NAME=wordpress" >> project/srcs/.env
-echo "DB_ROOT=rootpass" >> project/srcs/.env
-echo "DB_USER=wpuser" >> project/srcs/.env
-echo "DB_PASS=wppass" >> project/srcs/.env
-mkdir project/srcs/requirements/bonus
-mkdir project/srcs/requirements/mariadb
-mkdir project/srcs/requirements/mariadb/conf
-touch project/srcs/requirements/mariadb/conf/create_db.sh
-mkdir project/srcs/requirements/mariadb/tools
-echo "" > project/srcs/requirements/mariadb/tools/.gitkeep
-touch project/srcs/requirements/mariadb/Dockerfile
-touch project/srcs/requirements/mariadb/.dockerignore
-echo ".git" > project/srcs/requirements/mariadb/.dockerignore
-echo ".env" >> project/srcs/requirements/mariadb/.dockerignore
-mkdir project/srcs/requirements/nginx
-mkdir project/srcs/requirements/nginx/conf
-touch project/srcs/requirements/nginx/conf/nginx.conf
-mkdir project/srcs/requirements/nginx/tools
-touch project/srcs/requirements/nginx/Dockerfile
-echo ".git" > project/srcs/requirements/mariadb/.dockerignore
-echo ".env" >> project/srcs/requirements/mariadb/.dockerignore
-mkdir project/srcs/requirements/tools
-mkdir project/srcs/requirements/wordpress
-mkdir project/srcs/requirements/wordpress/conf
-touch project/srcs/requirements/wordpress/conf/wp-config-create.sh
-mkdir project/srcs/requirements/wordpress/tools
-echo "" > project/srcs/requirements/wordpress/tools/.gitkeep
-touch project/srcs/requirements/wordpress/Dockerfile
-touch project/srcs/requirements/wordpress/.dockerignore
-echo ".git" > project/srcs/requirements/wordpress/.dockerignore
-echo ".env" >> project/srcs/requirements/wordpress/.dockerignore
+mkdir ./srcs
+touch ./Makefile
+mkdir ./srcs/requirements
+touch ./srcs/docker-compose.yml
+touch ./srcs/.env
+echo "DOMAIN_NAME=agimi.42.fr" > ./srcs/.env
+echo "CERT_=./requirements/tools/agimi.42.fr.crt" >> ./srcs/.env
+echo "KEY_=./requirements/tools/agimi.42.fr.key" >> ./srcs/.env
+echo "DB_NAME=wordpress" >> ./srcs/.env
+echo "DB_ROOT=rootpass" >> ./srcs/.env
+echo "DB_USER=wpuser" >> ./srcs/.env
+echo "DB_PASS=wppass" >> ./srcs/.env
+mkdir ./srcs/requirements/bonus
+mkdir ./srcs/requirements/mariadb
+mkdir ./srcs/requirements/mariadb/conf
+touch ./srcs/requirements/mariadb/conf/create_db.sh
+mkdir ./srcs/requirements/mariadb/tools
+echo "" > ./srcs/requirements/mariadb/tools/.gitkeep
+touch ./srcs/requirements/mariadb/Dockerfile
+touch ./srcs/requirements/mariadb/.dockerignore
+echo ".git" > ./srcs/requirements/mariadb/.dockerignore
+echo ".env" >> ./srcs/requirements/mariadb/.dockerignore
+mkdir ./srcs/requirements/nginx
+mkdir ./srcs/requirements/nginx/conf
+touch ./srcs/requirements/nginx/conf/nginx.conf
+mkdir ./srcs/requirements/nginx/tools
+touch ./srcs/requirements/nginx/Dockerfile
+echo ".git" > ./srcs/requirements/mariadb/.dockerignore
+echo ".env" >> ./srcs/requirements/mariadb/.dockerignore
+mkdir ./srcs/requirements/tools
+mkdir ./srcs/requirements/wordpress
+mkdir ./srcs/requirements/wordpress/conf
+touch ./srcs/requirements/wordpress/conf/wp-config-create.sh
+mkdir ./srcs/requirements/wordpress/tools
+echo "" > ./srcs/requirements/wordpress/tools/.gitkeep
+touch ./srcs/requirements/wordpress/Dockerfile
+touch ./srcs/requirements/wordpress/.dockerignore
+echo ".git" > ./srcs/requirements/wordpress/.dockerignore
+echo ".env" >> ./srcs/requirements/wordpress/.dockerignore
