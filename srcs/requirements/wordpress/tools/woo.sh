731a03d09f5c9eae0d858b8bb2b21520e5de3b00
ThEWoLf882-42 <agimi@student.1337.ma>
2023-11-04 17:37:39 +0100

    redis

--- srcs/requirements/wordpress/tools/woo.sh
+++ srcs/requirements/wordpress/tools/woo.sh
@@ -2,7 +2,9 @@
 
 wp core install --path=/var/www --url=agimi.42.fr --title="${WP_TITLE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" --admin_email="${WP_EMAIL}"
 
-wp plugin is-installed redis-cache --path=/var/www --allow-root
+wp config set WP_REDIS_PORT "6379" --allow-root
+wp config set WP_REDIS_HOST "redis" --allow-root
+wp plugin is-installed redis-cache --allow-root
 
 if [ $? -eq 1 ]
 then
