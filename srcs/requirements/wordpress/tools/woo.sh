594f742af4593a7f11ee940cdfee5dd1a150a8da
ThEWoLf882-42 <agimi@student.1337.ma>
2023-11-04 17:14:24 +0100

    #!/bin/sh

--- srcs/requirements/wordpress/tools/woo.sh
+++ srcs/requirements/wordpress/tools/woo.sh
@@ -1,9 +1,15 @@
-wp core install --url=agimi.42.fr --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_PASS} --admin_email=${WP_EMAIL}
+#!/bin/sh
+
+wp core install --url=agimi.42.fr --title="${WP_TITLE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" --admin_email="${WP_EMAIL}"
+
 wp plugin is-installed redis-cache --allow-root
+
 if [ $? -eq 1 ]
 then
     wp plugin install redis-cache --force --activate --allow-root
 else
     echo "redis-cache is already installed"
 fi
-exec /usr/sbin/php-fpm8 -F
+
+# Start PHP-FPM in the foreground
+exec $@
