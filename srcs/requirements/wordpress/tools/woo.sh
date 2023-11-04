b535ab73da7041645e6161b37f0fdd1b416d41c3
ThEWoLf882-42 <agimi@student.1337.ma>
2023-11-04 17:19:31 +0100

    --path=/var/www

--- srcs/requirements/wordpress/tools/woo.sh
+++ srcs/requirements/wordpress/tools/woo.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-wp core install --url=agimi.42.fr --title="${WP_TITLE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" --admin_email="${WP_EMAIL}"
+wp core install --path=/var/www --url=agimi.42.fr --title="${WP_TITLE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" --admin_email="${WP_EMAIL}"
 
-wp plugin is-installed redis-cache --allow-root
+wp plugin is-installed redis-cache --path=/var/www --allow-root
 
 if [ $? -eq 1 ]
 then
-    wp plugin install redis-cache --force --activate --allow-root
+    wp plugin install redis-cache --path=/var/www --force --activate --allow-root
 else
     echo "redis-cache is already installed"
 fi
