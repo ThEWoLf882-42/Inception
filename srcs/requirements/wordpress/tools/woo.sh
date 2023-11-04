6f9d0db31c60ca324262e942dc4df121a08b4d13
ThEWoLf882-42 <agimi@student.1337.ma>
2023-11-04 17:08:00 +0100

    no if

--- srcs/requirements/wordpress/tools/woo.sh
+++ srcs/requirements/wordpress/tools/woo.sh
@@ -1,20 +1,9 @@
-echo Installing wordpress
-cd /var/www/
-ls /var/www/ | grep wp-config.php
-if [ $? -eq 0 ]
+wp core install --url=agimi.42.fr --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_PASS} --admin_email=${WP_EMAIL}
+wp plugin is-installed redis-cache --allow-root
+if [ $? -eq 1 ]
 then
-    echo "Found Wordpress, skipping"
+    wp plugin install redis-cache --force --activate --allow-root
 else
-    rm -rf *
-    wp core install --url=agimi.42.fr --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_PASS} --admin_email=${WP_EMAIL}
-    wp plugin is-installed redis-cache --allow-root
-    if [ $? -eq 1 ]
-    then
-        echo "redis-cache plugin is not installed, installing..."
-        wp plugin install redis-cache --force --activate --allow-root
-        echo "Redis-cache has been installed successfully"
-    else
-        echo "redis-cache is already installed"
-    fi
+    echo "redis-cache is already installed"
 fi
 exec $@
