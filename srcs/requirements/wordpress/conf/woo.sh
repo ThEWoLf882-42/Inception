9fa2736a432dbdc9cb735ef8ec292ade915f32ea
ThEWoLf882-42 <agimi@student.1337.ma>
2023-11-04 16:26:21 +0100

    & to $

--- srcs/requirements/wordpress/conf/woo.sh
+++ srcs/requirements/wordpress/conf/woo.sh
@@ -6,6 +6,6 @@ if [ $? -eq 0 ]
 then
     echo "Found Wordpress, skipping"
 else
-    wp core install --url=agimi.42.fr --title=WP_TITLE --admin_user=WP_ADMIN --admin_password=WP_PASS --admin_email=WP_EMAIL
+    wp core install --url=agimi.42.fr --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_PASS} --admin_email=${WP_EMAIL}
 	wp core install --url=https://hmeftah.42.fr --title="Sarah WP" --admin_user=${WP_ADM} --admin_password=${WP_ADM_PASS} --admin_email="${WP_ADM}"@42.fr --allow-root
 fi
