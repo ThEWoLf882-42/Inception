840d6f4905fca68218355a1fccc11b851907f500
ThEWoLf882-42 <agimi@student.1337.ma>
2023-12-23 14:55:55 +0100

    ho

--- srcs/requirements/wordpress/tools/woo.sh
+++ srcs/requirements/wordpress/tools/woo.sh
@@ -11,14 +11,20 @@ wp core install --path=/var/www --url=agimi.42.fr --title="${WP_TITLE}" --admin_
 
 wp user create "${WP_USER}" "${WP_UEMAIL}" --path=/var/www --user_pass="${WP_PASS}" --role=editor
 
-wp config set WP_REDIS_HOST redis --path=/var/www --allow-root
-wp config set WP_REDIS_PORT 6379 --path=/var/www --allow-root
-wp config set WP_REDIS_DATABASE 0 --path=/var/www --allow-root
-wp config set WP_CACHE --path=/var/www true --allow-root
 
-wp plugin update --path=/var/www --all --allow-root
-wp plugin install redis-cache --path=/var/www --force --activate --allow-root
-# wp redis enable --path=/var/www --allow-root
+
+
+wp config set WP_REDIS_PORT "6379" --path=/var/www --allow-root
+wp config set WP_REDIS_HOST "redis" --path=/var/www --allow-root
+wp config set WP_REDIS_DATABASE "0" --path=/var/www --allow-root
+wp plugin is-installed redis-cache --path=/var/www --allow-root
+if [ $? -eq 1 ]
+then
+        wp plugin install redis-cache --path=/var/www --force --activate --allow-root
+        # wp redis enable --path=/var/www --allow-root
+else
+        echo "redis-cache is already installed"
+
 
 # chown -R www-data:www-data /var/www/
 # chmod -R 777 /var/www/
