00367113c0d8b8a27243502eb61bfcd6b648999c
ThEWoLf882-42 <agimi@student.1337.ma>
2023-12-24 15:24:45 +0100

    --path=/var/www

--- srcs/requirements/wordpress/tools/woo.sh
+++ srcs/requirements/wordpress/tools/woo.sh
@@ -16,9 +16,9 @@ wp config set WP_REDIS_PORT "6379" --type=constant --path=/var/www --allow-root
 wp config set WP_REDIS_HOST "redis" --type=constant --path=/var/www --allow-root
 wp config set WP_REDIS_DATABASE "0" --type=constant --path=/var/www --allow-root
 
-wp plugin update --all --allow-root
-wp plugin install redis-cache --force --activate --allow-root
-wp redis enable --allow-root
+wp plugin update --path=/var/www --all --allow-root
+wp plugin install redis-cache --path=/var/www --force --activate --allow-root
+wp redis enable --path=/var/www --allow-root
 
 chown -R www-data:www-data /var/www/
 find /var/www/ -type d -exec chmod 755 {} \;
