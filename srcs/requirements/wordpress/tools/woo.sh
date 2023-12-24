9ad5cb0f836d8ea2fc42a46aca86aa6e8705702f
ThEWoLf882-42 <agimi@student.1337.ma>
2023-12-24 15:16:40 +0100

    tab

--- srcs/requirements/wordpress/tools/woo.sh
+++ srcs/requirements/wordpress/tools/woo.sh
@@ -1,29 +1,24 @@
 #!/bin/sh
 
-# Check if wp-config.php exists, if not create it.
 wp config create --path=/var/www \
-        --allow-root \
-        --dbname="${DB_NAME}" \
-        --dbuser="${DB_USER}" \
-        --dbpass="${DB_PASS}" \
-        --dbhost="mariadb"
+				--allow-root \
+				--dbname="${DB_NAME}" \
+				--dbuser="${DB_USER}" \
+				--dbpass="${DB_PASS}" \
+				--dbhost="mariadb"
 
 wp core install --path=/var/www --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" --admin_email="${WP_EMAIL}"
 
-# Check if a Wordpress user exists, if not create it.
 wp user create "${WP_USER}" "${WP_UEMAIL}" --path=/var/www --user_pass="${WP_PASS}" --role=editor --allow-root
 
-# Setup Redis configurations.
 wp config set WP_CACHE true --raw --type=constant --path=/var/www --allow-root
 wp config set WP_REDIS_PORT "6379" --type=constant --path=/var/www --allow-root
 wp config set WP_REDIS_HOST "redis" --type=constant --path=/var/www --allow-root
 wp config set WP_REDIS_DATABASE "0" --type=constant --path=/var/www --allow-root
 
-# Install and activate the Redis plugin if not already active.
 wp plugin install redis-cache --force --activate --allow-root
 wp redis enable --allow-root
 
-# Set proper permissions for Wordpress files.
 chown -R www-data:www-data /var/www/
 find /var/www/ -type d -exec chmod 755 {} \;
 find /var/www/ -type f -exec chmod 644 {} \;
