b5145f19261090e4a800c2a9c697b21e4e1935a1
ThEWoLf882-42 <agimi@student.1337.ma>
2023-12-24 19:15:10 +0100

    hffffff

--- srcs/requirements/wordpress/tools/woo.sh
+++ srcs/requirements/wordpress/tools/woo.sh
@@ -13,11 +13,6 @@ wp core install --path=/var/www --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --a
 
 wp user create "${WP_USER}" "${WP_UEMAIL}" --path=/var/www --user_pass="${WP_PASS}" --role=editor --allow-root
 
-wp config set DB_CHARSET utf8 --type=constant --path=/var/www --allow-root
-wp config set DB_COLLATE "" --type=constant --path=/var/www --allow-root
-wp config set WP_DEBUG true --type=constant --path=/var/www --allow-root
-wp config set WP_DEBUG_LOG true --type=constant --path=/var/www --allow-root
-wp config set WP_DEBUG_DISPLAY false --type=constant --path=/var/www --allow-root
 wp config set WP_MEMORY_LIMIT 256M --type=constant --path=/var/www --allow-root
 wp config set WP_CACHE true --raw --type=constant --path=/var/www --allow-root
 wp config set WP_REDIS_PORT 6379 --type=constant --path=/var/www --allow-root
