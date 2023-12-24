650365e1e27906a88cdfa1f0d4e50d2fc264be8e
ThEWoLf882-42 <agimi@student.1337.ma>
2023-12-24 17:20:16 +0100

    ok

--- srcs/requirements/wordpress/tools/woo.sh
+++ srcs/requirements/wordpress/tools/woo.sh
@@ -13,10 +13,10 @@ wp core install --path=/var/www --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --a
 
 wp user create "${WP_USER}" "${WP_UEMAIL}" --path=/var/www --user_pass="${WP_PASS}" --role=editor --allow-root
 
-# wp config set WP_DEBUG true --type=constant --path=/var/www --allow-root
-# wp config set WP_DEBUG_LOG true --type=constant --path=/var/www --allow-root
-# wp config set WP_DEBUG_DISPLAY false --type=constant --path=/var/www --allow-root
-# wp config set WP_MEMORY_LIMIT 256M --type=constant --path=/var/www --allow-root
+wp config set WP_DEBUG true --type=constant --path=/var/www --allow-root
+wp config set WP_DEBUG_LOG true --type=constant --path=/var/www --allow-root
+wp config set WP_DEBUG_DISPLAY false --type=constant --path=/var/www --allow-root
+wp config set WP_MEMORY_LIMIT 256M --type=constant --path=/var/www --allow-root
 wp config set WP_CACHE true --raw --type=constant --path=/var/www --allow-root
 wp config set WP_REDIS_PORT 6379 --type=constant --path=/var/www --allow-root
 wp config set WP_REDIS_HOST redis --type=constant --path=/var/www --allow-root
@@ -29,6 +29,6 @@ wp plugin install redis-cache --path=/var/www --force --activate --allow-root
 wp redis enable --path=/var/www --allow-root
 
 # chown -R www-data:www-data /var/www/
-chmod -R 755 /var/www/
+chmod -R 777 /var/www/
 
 exec $@
