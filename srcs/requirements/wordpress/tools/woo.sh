8f20f856e58e4e6c6c4898f0fd8301884fc6e762
ThEWoLf882-42 <agimi@student.1337.ma>
2023-12-25 09:24:30 +0100

    debug

--- srcs/requirements/wordpress/tools/woo.sh
+++ srcs/requirements/wordpress/tools/woo.sh
@@ -4,6 +4,10 @@ wp core install --path=/var/www --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --a
 
 wp user create "${WP_USER}" "${WP_UEMAIL}" --path=/var/www --user_pass="${WP_PASS}" --role=editor --allow-root
 
+wp config set WP_DEBUG true --type=constant --path=/var/www --allow-root
+wp config set WP_DEBUG_LOG true --type=constant --path=/var/www --allow-root
+wp config set WP_DEBUG_DISPLAY false --type=constant --path=/var/www --allow-root
+
 wp plugin update --path=/var/www --all --allow-root
 wp plugin install redis-cache --path=/var/www --force --activate --allow-root
 wp redis enable --path=/var/www --allow-root
